b37105767a345439d43a022e1e01652e1765a80d
agarwalprasun76 <agarwalprasun@gmail.com>
2026-02-21 22:48:53 -0500

    Implement user feedback improvements in Settings component, enhancing password confirmation and protection level selection functionality

---
+++ admin_ui/src/api/index.ts
@@ -0,0 +1,8 @@
+export * as authApi from "./auth";
+export { requestJson, ApiClientError } from "./client";
+export * as dashboardApi from "./dashboard";
+export * as metaApi from "./meta";
+export * as devicesApi from "./devices";
+export * as exceptionsApi from "./exceptions";
+export * as settingsApi from "./settings";
+export { getAuthToken, setAuthToken, clearAuthToken } from "./token";
